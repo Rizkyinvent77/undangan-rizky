af7ba04203b2fdf82effc51b7940434d17179d20
Rizkyinvent77 <312550096+Rizkyinvent77@users.noreply.github.com>
2026-08-05 22:46:02 +0700

    Refactor wishes.js for cleaner code structure

--- js/wishes.js
+++ js/wishes.js
@@ -1,68 +1,41 @@
 import { db } from "./firebase.js";
 
 import {
-
     collection,
-
     addDoc,
-
-    serverTimestamp,
-
-    query,
-
-    orderBy,
-
-    onSnapshot
-
-} 
-
-from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const form = document.getElementById("wishForm");
-
-const wishList = document.getElementById("wishList");
-
 const wishesRef = collection(db, "wishes");
 
-/* ==========================
-      SEND WISH
-========================== */
-
 form.addEventListener("submit", async (e) => {
-
-console.log("FORM DIKIRIM");
-alert("FORM DIKIRIM");
 
     e.preventDefault();
 
     const name = document.getElementById("name").value.trim();
-
     const message = document.getElementById("message").value.trim();
 
-    if(!name || !message) return;
+    if (!name || !message) return;
 
-    try{
+    try {
 
-        await addDoc(wishesRef,{
-
-            name,
-
-            message,
-
+        await addDoc(wishesRef, {
+            name: name,
+            message: message,
             createdAt: serverTimestamp()
-
         });
 
         form.reset();
 
         alert("Terima kasih atas doa dan ucapannya 💚");
 
-    }catch(error){
+    } catch (error) {
 
-    console.error(error);
+        console.error(error);
 
-    alert(error.message);
+        alert(error.message);
 
-}
+    }
 
 });
