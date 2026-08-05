5fd465d8c1234380691dccfbb7be0c4f5626ba1b
Rizkyinvent77 <312550096+Rizkyinvent77@users.noreply.github.com>
2026-08-05 23:12:36 +0700

    cfgg

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+import {
+    initializeFirestore
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHVp0CEn_wzFbnuiVopzF98OshcKDBlks",
-  authDomain: "rizky-wedding.firebaseapp.com",
-  projectId: "rizky-wedding",
-  storageBucket: "rizky-wedding.firebasestorage.app",
-  messagingSenderId: "158026459321",
-  appId: "1:158026459321:web:abcada7fd50e66ec6f1368"
+    apiKey: "AIzaSyBHVp0CEn_wzFbnuiVopzF98OshcKDBlks",
+    authDomain: "rizky-wedding.firebaseapp.com",
+    projectId: "rizky-wedding",
+    storageBucket: "rizky-wedding.firebasestorage.app",
+    messagingSenderId: "158026459321",
+    appId: "1:158026459321:web:abcada7fd50e66ec6f1368"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+    useFetchStreams: false
+});
