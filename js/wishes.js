f341b93fa5d5c0081ebeb4e631cd8ed82eb8f479
Rizkyinvent77 <312550096+Rizkyinvent77@users.noreply.github.com>
2026-08-06 13:41:28 +0700

    Add wish list and card styles to CSS file

--- js/wishes.js
+++ js/wishes.js
@@ -3,12 +3,16 @@ import { db } from "./firebase.js";
 import {
     ref,
     push,
-    set, 
+    set,
+    onValue
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 
 
 const form = document.getElementById("wishForm");
 
+const wishList = document.getElementById("wishList");
+
+const wishTitle = document.getElementById("wishTitle");
 
 const wishesRef = ref(db, "wishes");
 
@@ -60,5 +64,99 @@ form.addEventListener("submit", async (e)=>{
 
     }
 
+
+});
+
+function timeAgo(timestamp){
+
+    const seconds = Math.floor((Date.now() - timestamp)/1000);
+
+    if(seconds < 60) return "Baru saja";
+
+    const minutes = Math.floor(seconds/60);
+
+    if(minutes < 60)
+        return `${minutes} menit yang lalu`;
+
+    const hours = Math.floor(minutes/60);
+
+    if(hours < 24)
+        return `${hours} jam yang lalu`;
+
+    const days = Math.floor(hours/24);
+
+    return `${days} hari yang lalu`;
+
+}
+
+function getAvatarColor(name){
+
+    const colors = [
+        "#6B8F71",
+        "#6C8AE4",
+        "#C47AC0",
+        "#F0A04B",
+        "#4DB6AC",
+        "#E57373",
+        "#9575CD",
+        "#64B5F6"
+    ];
+
+    let total = 0;
+
+    for(let i = 0; i < name.length; i++){
+        total += name.charCodeAt(i);
+    }
+
+    return colors[total % colors.length];
+}
+
+onValue(wishesRef, (snapshot) => {
+
+    wishList.innerHTML = "";
+
+    if (!snapshot.exists()) return;
+
+    const data = snapshot.val();
+
+    const wishes = Object.values(data).reverse();
+    
+    wishTitle.textContent = `Ucapan & Doa (${wishes.length})`;
+
+    wishes.forEach((wish) => {
+
+        const time = timeAgo(wish.createdAt);
+        
+        wishList.innerHTML += `
+
+        <div class="wish-card">
+
+            <div class="wish-header">
+
+                <div
+    class="avatar"
+    style="background:${getAvatarColor(wish.name)};">
+
+    ${wish.name.charAt(0).toUpperCase()}
+
+</div>
+
+                <div>
+
+                    <h3>${wish.name}</h3>
+
+                    <span>${time}</span>
+
+                </div>
+
+            </div>
+
+            <p>${wish.message}</p>
+
+        </div>
+
+        `;
+
+    });
 
 });
